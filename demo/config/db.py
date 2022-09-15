3a38428a377725f9f8b13de9af82969629ee34d0
joacota2 <gomezjoaquin83@gmail.com>
2022-09-15 12:42:57 -0300

    comiteca

--- demo/config/db.py
+++
@@ -1,9 +0,0 @@
-from venv import create
-from sqlalchemy import create_engine, MetaData
-
-engine = create_engine("mysql+pymysql://root:administrador@localhost:3306/pi_individual")
-#engine = create_engine("mysql+pymysql://u653914786_henrypi123:Administrador1@45.132.157.154:3306/u653914786_henrypi")
-
-meta = MetaData()
-
-conn = engine.connect()
